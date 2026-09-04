4c605b5f9e9109ab9e80a396080dc1766d122dc3
Pedro Santana <pmendes09@hotmail.com>
2026-09-04 14:07:12 -0300

    feat: alinhar modelos ao schema do banco

--- backend/app/models/db_types.py
+++ backend/app/models/db_types.py
@@ -1,10 +1,12 @@
 """Tipos PostgreSQL sem equivalente nativo direto no SQLAlchemy."""
 
+from typing import Any, cast
+
 from sqlalchemy.dialects.postgresql.base import ischema_names
-from sqlalchemy.types import UserDefinedType
+from sqlalchemy.types import TypeEngine, UserDefinedType
 
 
-class PostgreSQLPoint(UserDefinedType):
+class PostgreSQLPoint(UserDefinedType[object]):
     """Tipo geométrico POINT usado pelo dump de cidades do IBGE."""
 
     cache_ok = True
@@ -13,5 +15,7 @@ class PostgreSQLPoint(UserDefinedType):
         return "POINT"
 
 
-ischema_names.setdefault("point", PostgreSQLPoint)
+cast(dict[str, type[TypeEngine[Any]]], ischema_names).setdefault(
+    "point", PostgreSQLPoint
+)
 PG_POINT = PostgreSQLPoint()
